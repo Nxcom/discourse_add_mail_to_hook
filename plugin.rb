3105b73e5f7c8ebaebc2e2133fb591dd95c37b1f
hatuan195 <47517542+hatuan195@users.noreply.github.com>
2024-09-06 10:41:47 +0700

    update

--- plugin.rb
+++ plugin.rb
@@ -3,15 +3,29 @@
 # version: 0.1
 # authors: TuanHA
 
+# after_initialize do
+#   # Custom webhook modification
+#   DiscourseEvent.on(:post_created) do |event|
+#     # Assuming event contains the user's ID
+#     if event[:user_id]
+#       user = User.find_by(id: event[:user_id])
+#       if user
+#         event[:user_email] = user.email
+#       end
+#     end
+#   end
+# end
+
 after_initialize do
-  # Custom webhook modification
-  DiscourseEvent.on(:post_created) do |event|
-    # Assuming event contains the user's ID
-    if event[:user_id]
-      user = User.find_by(id: event[:user_id])
-      if user
-        event[:user_email] = user.email
-      end
+  # Extend the WebHookPostView to include the user's email in the payload
+  module ::CustomWebHookExtension
+    def email
+      object.user.email
     end
+  end
+
+  # Add the email field to the webhook post payload
+  add_to_serializer(:web_hook_post, :user_email) do
+    object.user.email if object.user
   end
 end
