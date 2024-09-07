c3ebbc3c92f6b5acbbe78b44fad4e5203ff78a56
hatuan195 <47517542+hatuan195@users.noreply.github.com>
2024-09-07 12:06:47 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,16 +3,13 @@
 # version: 0.1
 # authors: TuanHA
 
-#after_initialize do
-  # Extend the WebHookPostView to include the user's email in the payload
-#  module ::CustomWebHookExtension
-#    def email
-#      object.user.email
-#    end
-#  end
-
-
 after_initialize do
+  # Extend the WebHookPostView to include the user's email in the payload
+  module ::CustomWebHookExtension
+    def email
+      object.user.email
+    end
+  end
   # Add the email field to the webhook post payload
   add_to_serializer(:web_hook_post, :user_email) do
     object.user.email if object.user
