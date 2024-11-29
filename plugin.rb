fb899c22ca9536d569e061da07e2601f706e2c80
hatuan195 <47517542+hatuan195@users.noreply.github.com>
2024-11-29 13:48:33 +0700

    update

--- plugin.rb
+++ plugin.rb
@@ -1,31 +1,31 @@
-# name: custom-webhook-with-email
-# about: Adds user email to the webhook payload
-# version: 0.1
+# name: custom-webhook-with-email-and-username
+# about: Adds user email and username to the webhook payload
+# version: 0.2
 # authors: TuanHA
 
 after_initialize do
-  # Add the email field to the webhook post payload
+  # Add the email and username fields to the webhook post payload
   add_to_serializer(:web_hook_post, :post_user_email) do
     object.user.email if object.user
   end
-
-  # Add the email field to the notification webhook payload
-  add_to_serializer(:notification, :notified_user_email) do
-    #Rails.logger.info("Notification Object: #{object.inspect}")
-    user = User.find_by(id: object[:user_id])
-    if user
-      #Rails.logger.info("User Object: #{user.inspect}")
-      user.email
-    else
-      #Rails.logger.warn("No user found for Notification.")
-      nil
-    end
-  end
   
+  add_to_serializer(:web_hook_post, :post_user_username) do
+    object.user.username if object.user
+  end
+
+  # Add the email and username fields to the notification webhook payload
+  add_to_serializer(:notification, :notified_user_email) do
+    user = User.find_by(id: object[:user_id])
+    user&.email
+  end
+
+  add_to_serializer(:notification, :notified_user_username) do
+    user = User.find_by(id: object[:user_id])
+    user&.username
+  end
+
   # Add the base URL to all webhook payloads, regardless of type
   add_to_serializer(:application, :site_url) do
     Discourse.base_url
   end
-
 end
-
